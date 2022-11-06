8ad4390ad90c15b362d69d4fee297466ac7e2b0d
Maxximusan <dragonstonecave@gmail.com>
2022-11-06 14:47:08 +0200

    5й наброски логики...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,18 +9,26 @@ import { Searchbar } from 'components/Searchbar/Searchbar'
 
 export class App extends React.Component {
   state = {
-    data: null,
-    searchQuery: '',
-    page: 1
+   
+    searchQuery: 'q',
+    page: 1,
+    images: null,
+    totalHits: 1,
+    isLoaded: false,
+    error:'',
+    
   }
   componentDidMount() {
-    apiFetchGallery('dog').then(result => {
-      const findQuery = result.data
-      console.log(findQuery);
-      this.setState({ data: findQuery}) 
-      console.log(this.state.data);
-    })
-     console.log(this.state.data);
+    
+    console.log(this.state.searchQuery);
+    // apiFetchGallery('dog').then(result => {
+    //   const findQuery = result.data
+    //   console.log(findQuery);
+    //   console.log(findQuery.hits);
+    //   this.setState({ data: findQuery}) 
+    //   console.log(this.state.data);
+    // })
+     
     // fetch('https://pixabay.com/api/?q=cat&page=1&key=30128304-708965977259e04966a50b0c9&image_type=photo&orientation=horizontal')
     //      .then(res => res.json())
     //   .then(console.log)
@@ -28,8 +36,54 @@ export class App extends React.Component {
     
   }
 
-  componentDidUpdate() { }
   
+  componentDidUpdate(prevProps, prevState) { 
+    const prevImages = prevState.searchQuery;
+    const nextImages = this.state.searchQuery;
+    const prevPage = prevState.page;
+    const nextPage = this.state.page;
+    console.log(prevImages);
+    console.log(nextImages);
+    console.log(prevPage);
+    console.log(nextPage);
+    if (prevImages !== nextImages) {
+      
+      
+      this.setState({
+        
+        page: 1,
+        images: [],
+      });
+      this.fetchGallery(nextImages, nextPage);
+    }
+
+    if (prevPage !== nextPage && nextPage !== 1) {
+      this.fetchGallery(nextImages, nextPage);
+    }
+
+  }
+  
+  fetchGallery(nextImages, nextPage) {
+    apiFetchGallery(nextImages, nextPage)
+      .then(
+        (result) => {
+          this.setState(prevState => {
+            return {
+              isLoaded: true,
+              images: [...prevState.images, ...result.data.hits],
+              prevState,
+              totalHits: result.data.totalHits,
+              searchQuery: nextImages
+            };
+          });
+          console.log(this.state.images);
+          console.log(this.state.totalHits);
+          console.log(this.state.searchQuery);
+        })
+    .catch(error => this.setState({error}))
+
+  }
+
 
   onSubmit = searchQuery => {
     this.setState({ searchQuery, page: 1 });
